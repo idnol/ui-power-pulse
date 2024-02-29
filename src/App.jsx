0e0017a8f4167ffce7aa2bbf3d65d2c1236a7249
Olesia511 <131859369+Olesia511@users.noreply.github.com>
2024-02-29 17:05:18 +0200

    routes exercises detailes

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,41 @@
 import { Route, Routes } from 'react-router-dom';
 import { AppLayout } from './components/AppLayout/AppLayout.jsx';
-import { lazy } from 'react';
+import { Suspense, lazy } from 'react';
 
 const WelcomePage = lazy(() => import('./pages/WelcomePage/WelcomePage.jsx'));
 const SigninPage = lazy(() => import('./pages/SigninPage/SigninPage.jsx'));
 const SignupPage = lazy(() => import('./pages/SignupPage/SignupPage.jsx'));
 const ProfilePage = lazy(() => import('./pages/ProfilePage/ProfilePage.jsx'));
 const DiaryPage = lazy(() => import('./pages/DiaryPage/DiaryPage.jsx'));
-const ExercisesPage = lazy(() => import('./pages/ExercisesPage/ExercisesPage.jsx'));
-const ProductsPage = lazy(() => import('./pages/ProductsPage/ProductsPage.jsx'));
+const ExercisesPage = lazy(() =>
+  import('./pages/ExercisesPage/ExercisesPage.jsx')
+);
+
+const ProductsPage = lazy(() =>
+  import('./pages/ProductsPage/ProductsPage.jsx')
+);
 const ErrorPage = lazy(() => import('./pages/ErrorPage/ErrorPage.jsx'));
 
 function App() {
-
   return (
-    <Routes>
-      <Route path="/" element={<AppLayout />}>
-        <Route index element={<WelcomePage />} />
-        <Route path="signin" element={<SigninPage />} />
-        <Route path="signup" element={<SignupPage />} />
-        <Route path="profile" element={<ProfilePage />} />
-        <Route path="diary" element={<DiaryPage />} />
-        <Route path="exercises" element={<ExercisesPage />} />
-        <Route path="products" element={<ProductsPage />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<WelcomePage />} />
+          <Route path="signin" element={<SigninPage />} />
+          <Route path="signup" element={<SignupPage />} />
+          <Route path="profile" element={<ProfilePage />} />
+          <Route path="diary" element={<DiaryPage />} />
+          <Route path="exercises/" element={<ExercisesPage />}>
+            <Route path="bodyparts" element={<div>Body parts</div>} />
+            <Route path="muscles" element={<div>Muscles</div>} />
+            <Route path="equipment" element={<div>Equipment</div>} />
+          </Route>
+          <Route path="products" element={<ProductsPage />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 export default App;
--- src/pages/ExercisesPage/ExercisesPage.jsx
+++ src/pages/ExercisesPage/ExercisesPage.jsx
@@ -1,3 +1,22 @@
+import { NavLink, Outlet } from 'react-router-dom';
+
 export default function ExercisesPage() {
-  return <><h1>exercises</h1></>
+  return (
+    <>
+      <h1>exercises</h1>
+      <ul>
+        <li>
+          <NavLink to="bodyparts">Body parts</NavLink>
+        </li>
+        <li>
+          <NavLink to="muscles">Muscles</NavLink>
+        </li>
+        <li>
+          <NavLink to="equipment">Equipment</NavLink>
+        </li>
+      </ul>
+
+      <Outlet />
+    </>
+  );
 }
