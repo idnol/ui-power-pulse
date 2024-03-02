f80fc7abb5a2b81a4faba60750c9520fe8ff9321
Oksana Jarosch <ok.jarosch@gmail.com>
2024-03-02 02:08:11 +0100

    SignInForm

---
+++ src/components/AuthForms/SignInForm.jsx
@@ -0,0 +1,48 @@
+import { Form, Formik } from 'formik';
+import * as Yup from 'yup';
+import { Error, StyledButton, StyledDiv, StyledGreetings, StyledInput, StyledLink, StyledSecondaryText, StyledTitle } from './AuthForms.styled';
+
+
+const schema = Yup.object().shape({
+    email: Yup.string().matches(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/, "It must be valid email").required('Required'),
+    password: Yup.string().min(6, "password must be at least 6 characters").required('Required')
+});
+
+export const SignInForm = () => {
+
+    const handleLogin = (values) => {
+        // dispatch(login(values));
+        console.log(values);
+    }
+
+    return (
+        <>
+
+            <StyledDiv>
+                <StyledTitle>Sign In</StyledTitle>
+                <StyledGreetings>Welcome! Please enter your credentials to login to the platform:</StyledGreetings>
+
+                <Formik
+                    initialValues={{
+                        email: '',
+                        password: '',
+                    }}
+                    validationSchema={schema}
+                    onSubmit={(values) => {
+                        handleLogin(values);
+                    }}>
+                    <Form autoComplete="off">
+                        <StyledInput type="email" name="email" placeholder="Email" />
+                        <Error name="email" component="p" />
+                        <StyledInput type="password" name="password" placeholder="Password" />
+                        <Error name="password" component="p" />
+                        <StyledButton type="submit">Sign Up</StyledButton>
+                    </Form>
+                </Formik>
+
+                <StyledSecondaryText>Don’t have an account? <StyledLink to="/signup">Sign Up</StyledLink></StyledSecondaryText>
+            </StyledDiv>
+
+        </>
+    )
+}
