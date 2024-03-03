e837b298e1a86945499c3be996d7f769e1f79fd5
oxananazaruk <132213141+oxananazaruk@users.noreply.github.com>
2024-03-03 18:07:45 +0200

    fixed

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/react_vite">
-        <App />
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
 );
