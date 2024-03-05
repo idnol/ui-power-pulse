ad70cfff70880ff7ef2131e01d2d0ea34339d49e
Olesia511 <131859369+Olesia511@users.noreply.github.com>
2024-03-05 11:16:53 +0200

    fix

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,11 +1,7 @@
-import StyledDatepicker from '../../components/DatePicker/StyledDatepicker';
-// import StyledDatepicker from '../../components/DatePicker/DatePicker';
-
 export default function WelcomePage() {
   return (
     <>
       <h1>welcome</h1>
-      <StyledDatepicker />
     </>
   );
 }
