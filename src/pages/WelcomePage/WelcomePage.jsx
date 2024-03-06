69bf397d8f9d76791caed0e70004fd4f254929e2
Olesia511 <131859369+Olesia511@users.noreply.github.com>
2024-03-06 13:44:44 +0200

    added StatisticsInfo

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,9 +1,11 @@
 import { Welcome } from '../../components/Welcome/Welcome';
+import { StatisticsInfo } from '../../components/StatisticsInfo/StatisticsInfo';
 
 export default function WelcomePage() {
   return (
     <>
       <Welcome />
+      <StatisticsInfo/>
     </>
   );
 }
