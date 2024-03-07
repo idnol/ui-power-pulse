b935e84f256960c256822c0690c65c8d599e6590
Ivan Bilenkyi <ivanbilenkyi@gmail.com>
2024-03-07 17:15:46 +0200

    fix logout

--- src/components/Header/closeMenu.js
+++ src/components/Header/closeMenu.js
@@ -1,11 +1,4 @@
-import { logout } from '../../redux/auth/api.js';
-import { useDispatch } from 'react-redux';
-
-
 export const closeMenu = () => {
-  console.log(1);
-  const dispatch = useDispatch()
-  console.log(4);
   const menu = document.getElementById('menu-wrapper');
   const backdrop = document.getElementById('backdrop');
   !backdrop.style.left || backdrop.style.left === '-100%'
@@ -14,7 +7,4 @@ export const closeMenu = () => {
   menu.style.visibility = 'hidden';
   menu.style.opacity = 0;
   menu.style.display = 'none';
-  console.log(5);
-  dispatch(logout())
-
 };
