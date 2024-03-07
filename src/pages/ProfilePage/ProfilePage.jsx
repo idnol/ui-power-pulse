c519875120df7096308ae1dd0265c3cd3eddfb43
Dmytro Shkatula <idnol84@gmail.com>
2024-03-07 17:01:38 +0200

    updated project

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,12 +1,22 @@
 import { UserCard } from '../../components/ProfileSettings/UserCard/UserCard';
 import { UserForm } from '../../components/ProfileSettings/UserForm/UserForm';
 import { PageWrapper } from './ProfilePage.styled';
+import { Wrapper } from '../WelcomePage/WelcomePage.styled.js';
 
 export default function ProfilePage() {
     return (
-        <PageWrapper>
-            <UserCard />
-            <UserForm />
-        </PageWrapper>
+      <section>
+        <div className="container">
+          <div className="row">
+            <Wrapper>
+              <PageWrapper>
+                <UserCard />
+                <UserForm />
+              </PageWrapper>
+            </Wrapper>
+          </div>
+        </div>
+      </section>
+
   );
 }
