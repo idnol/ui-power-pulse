e0485a1958ca4b2329fe32830d858ce952f78ad8
Oksana Jarosch <ok.jarosch@gmail.com>
2024-03-07 22:21:38 +0100

    PrivateRoute

---
+++ src/components/Routes/PrivateRoute.jsx
@@ -0,0 +1,13 @@
+import { useSelector } from "react-redux"
+import { Navigate } from "react-router-dom";
+import { selectIsLoggedin, selectIsRefreshing } from "../../redux/auth/selectors";
+
+
+export const PrivateRoute = ({component: Component, redirectTo = "/"}) => {
+    
+    const isLoggedin = useSelector(selectIsLoggedin);
+    const isRefreshing = useSelector(selectIsRefreshing);
+    const shouldRedirect = !isLoggedin && !isRefreshing;
+
+    return shouldRedirect ? (<Navigate to={redirectTo} />) : Component;
+}
