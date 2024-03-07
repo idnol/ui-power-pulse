ca8d54023d862697b6e7bead9bf42c900498b58a
oxananazaruk <132213141+oxananazaruk@users.noreply.github.com>
2024-03-07 21:08:19 +0200

    Update api.js

--- src/redux/products/api.js
+++ src/redux/products/api.js
@@ -1,16 +1,11 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
-// import { setAuthHeader } from '../auth/api';
-
-// const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ZTVlZGY3NjAwYzQ4ZjY3MTc5YzMxOCIsImlhdCI6MTcwOTgwNTIyOCwiZXhwIjoxNzA5ODg4MDI4fQ.2RE9n4waW-F7l30e5B9SJ81uB1pxPXbEbdUBjJ7LKcA"
-
-// axios.defaults.baseURL = 'http://localhost:3333/api/products';
 
 export const fetchProducts = createAsyncThunk(
   'products/fetchProducts',
   async (searchParams, thunkAPI) => {
     try {
-      const response = await axios.get('/', { params: searchParams });
+      const response = await axios.get('products/', { params: searchParams });
       return response.data;
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
@@ -19,6 +14,6 @@ export const fetchProducts = createAsyncThunk(
 );
 
 export const fetchCategories = async () => {
-  const response = await axios.get(`/categories`);
+  const response = await axios.get(`products/categories`);
   return response.data;
 };
