3c9e86e8c95e6aabe75dc589361d339018b4a53c
Dmytro Shkatula <idnol84@gmail.com>
2024-03-11 16:32:51 +0200

    updated project

--- src/redux/diary/diarySelectors.js
+++ src/redux/diary/diarySelectors.js
@@ -1,3 +1,5 @@
 export const selectSuccess = state => state.diary.isSuccess;
 
 export const selectError = state => state.diary.error;
+
+export const selectProducts = state => state.diary.products;
