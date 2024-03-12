04a5a9c496819d0e281511ecc147676b4f658e7b
oxananazaruk <132213141+oxananazaruk@users.noreply.github.com>
2024-03-12 18:55:04 +0200

    styles scroll

--- src/components/ProductsList/ProductsList.styled.js
+++ src/components/ProductsList/ProductsList.styled.js
@@ -5,8 +5,29 @@ export const ProductsContainer = styled.div`
   max-width: 335px;
   margin: 0 auto;
   @media (min-width: ${theme.breakpoint.md}) {
-    max-width: 686px;
+    max-width: 704px;
     margin: 0;
+    overflow-y: scroll;
+    height: 660px;
+    @-moz-document url-prefix() {
+      scrollbar {
+      width: thin;
+      color: ${theme.color.orange};
+      background-color: ${theme.color.greySixth};
+      }
+    }
+    &::-webkit-scrollbar {
+      width: 8px;
+    }
+    &::-webkit-scrollbar-track {
+      background-color: ${theme.color.greySixth};
+      border-radius: 12px;
+    } 
+    &::-webkit-scrollbar-thumb {
+      background-color: ${theme.color.orange};
+      border-radius: 12px;
+      height: 153px;
+    }
   }
   @media (min-width: ${theme.breakpoint.xl}) {
     max-width: 850px;
