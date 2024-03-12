722bf98a875d5e9b79b1cad1474075ac721510e8
NikVio <131026216+NikVio@users.noreply.github.com>
2024-03-13 01:11:59 +0200

    close menu after click logout

--- src/components/parts/LogOut/LogOut.jsx
+++ src/components/parts/LogOut/LogOut.jsx
@@ -3,12 +3,15 @@ import { LogOutLink, LogOutIcon } from './LogOut.styled';
 import { theme } from '/src/vars.js';
 import { useDispatch } from 'react-redux';
 import { logout } from '../../../redux/auth/api.js';
+import { closeMenu } from '../../Header/closeMenu';
+
 
 
 export const LogOut = ({color = theme.color.white, hovercolor=`none`, stroke = theme.color.orange}) => {
   const dispatch = useDispatch()
   const handleLogout = ()=>{
     dispatch(logout())
+    closeMenu()
   }
   return (
     <LogOutLink color={color} hovercolor={hovercolor} to={'/'} onClick={handleLogout}>
